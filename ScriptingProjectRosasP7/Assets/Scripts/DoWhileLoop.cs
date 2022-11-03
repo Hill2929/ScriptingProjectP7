1be61d4cbeb00fd34cbf720750bedab7ee0cff39
Hill2929 <736120@student.dusd.net>
2022-11-03 14:12:06 -0700

    Did somethings like move rubys coordinates
    Moving ruby's coordinates  and etc..

---
+++ ScriptingProjectRosasP7/Assets/Scripts/DoWhileLoop.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoWhileLoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool shoulddContinue = false;
+
+        do
+        {
+            print("Hello World");
+
+        } while (shoulddContinue == true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
